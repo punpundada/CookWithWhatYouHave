4bcd30f31ca7b63a30f8714def6ad2d0830a6799
Parashkar prajwal <prajwalparashkar100@gmail.com>
2023-09-03 00:39:46 +0530

    Minor Changes

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,9 +2,10 @@ import Login from "./Login";
 import SignUp from "./SignUp";
 import DashBoard from "./DashBoard";
 import Home from "./Home";
+
 export {
     Login , 
     SignUp ,
     DashBoard,
     Home
-}
+};
