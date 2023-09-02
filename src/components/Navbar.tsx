0463936a3c6598f0de5c878c6619530d1a4875b3
Parashkar prajwal <prajwalparashkar100@gmail.com>
2023-09-02 19:40:54 +0530

    NavBar created

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import { navLinks } from '../constants/NavItems'
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className='w-screen h-11 bg-gray-900 text-white'>
-      
+        hello
+        <ul className='flex gap-2'>
+            {
+                navLinks.map((item)=>(
+                    <li key={item.id}>
+                        <Link to={'/'} >{item.label}</Link>
+                    </li>
+                ))
+            }
+        </ul>
     </div>
   )
 }
