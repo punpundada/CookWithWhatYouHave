57cd817a666974496747e57ccdd143f3e27d537e
Parashkar prajwal <prajwalparashkar100@gmail.com>
2023-09-02 19:31:16 +0530

    Added basic wireframe for project

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-screen h-11 bg-gray-900 text-white'>
+      
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/constants/NavItems.tsx
@@ -0,0 +1,17 @@
+export const navLinks=[
+    {
+        id:1,
+        name:'home',
+        label:'Home'
+    },
+    {
+        id:2,
+        name:'login',
+        label:'LogIn'
+    },
+    {
+        id:2,
+        name:'aboutUs',
+        label:'About us'
+    },
+]
